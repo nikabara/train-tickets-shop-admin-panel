b6b42be5019608f25c218e62c2ce37e2e350bdbe
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-12-24 23:35:02 +0400

    Added designs and admin paenl ty shi XD validation of ticket added download ticket invoice print onvoice and delete invoice buttons and some designs to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { FirebaseService } from './services/firebase.service';
 import { Timestamp } from '@angular/fire/firestore';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     RouterModule,
-    SidebarComponent
+    SidebarComponent,
+    CommonModule
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
 
-  constructor(private firebaseService: FirebaseService) { }
+  constructor(private firebaseService: FirebaseService, private router: Router) { }
 
+  isLoginRoute: boolean = false;
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.isLoginRoute = this.router.url === '/log-in' || this.router.url === '' || this.router.url === '/';
+    });
+  }
 }
--- src/app/components/validate-ticket/validate-ticket.component.ts
+++ src/app/components/validate-ticket/validate-ticket.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { SwaggerApiService } from './../../services/swagger-api.service';
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
 
 @Component({
   selector: 'app-validate-ticket',
@@ -11,6 +14,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class ValidateTicketComponent {
   private swaggerApiService: SwaggerApiService = inject(SwaggerApiService);
+
+  @ViewChild('pdfContent', { static: false }) pdfContent!: ElementRef;
 
   ticketId: string = '';
 
@@ -24,5 +29,47 @@ export class ValidateTicketComponent {
         console.log(this.ticketInfo);
       }
     )
+  }
+
+  totalPrice(): number {
+    let sum: number = 0;
+
+    for (let i = 0; i < this.ticketInfo.persons.length; i++) {
+      sum += this.ticketInfo.persons[i].seat.price;
+    }
+
+    return sum;
+  }
+
+  generatePDF() {
+    const element = this.pdfContent.nativeElement;
+
+    html2canvas(element).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const imgWidth = 190; // A4 page width in mm
+      const pageHeight = 277; // A4 page height in mm
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      let position = 20;
+
+      // Add the image to the PDF
+      pdf.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+
+      // Save the PDF
+      pdf.save('invoice.pdf');
+    });
+  }
+
+  print() {
+    const printContents = this.pdfContent.nativeElement.innerHTML;
+    const originalContents = document.body.innerHTML;
+
+    // Replace the body content with the section to print
+    document.body.innerHTML = printContents;
+    window.print();
+
+    // Restore the original body content after printing
+    document.body.innerHTML = originalContents;
   }
 }
